5f7387c439aaaaf03a1fa747f797f46d6d30ca94
Teterwise_bitbucket <teterwise.bitbucket@gmail.com>
2023-05-05 13:35:21 +0530

    test

--- src/Store/Actions/AuthActions.js
+++
@@ -1,8 +0,0 @@
-import { notification } from "antd"
-
-const apiSuccessNew = (response) => notification['success']({ description: response?.message || 'Success!' });
-const apiErrorNew = (err) => notification['error']({ description: err?.message || 'Something is wrong!' });
-
-export const login = (payload, callback) => (dispatch) => {
-
-};
--- src/Store/Reducers/AuthReducers.js
+++
@@ -1,9 +0,0 @@
-export function authReducer(state = {}, action) {
-
-  switch (action.type) {
-    
-    default:
-      return state;
-  }
-
-}
